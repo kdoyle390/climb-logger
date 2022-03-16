e9de12e967bff36c5116eeb01163134a311d621b
Kelsey <kdoyle390@gmail.com>
2022-03-16 10:14:26 -0500

    uncommenting seed file for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,26 +9,26 @@ require 'faker'
 # #   Character.create(name: 'Luke', movie: movies.first)
 
 
-# (1..5).each do |id|
-#     User.create!(
-#         id: id, 
-#         first_name: Faker::Name.first_name,
-#         last_name: Faker::Name.last_name,
-#         email: Faker::Internet.email, 
-#         password: "password"
+(1..5).each do |id|
+    User.create!(
+        id: id, 
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        email: Faker::Internet.email, 
+        password: "password"
 
-#     )
-# end
-
-
-# grades = Grade.create([{ grade: 'VB', type_id: 1}, { grade: 'V0', type_id: 1}, { grade: 'V1', type_id: 1}, { grade: 'V2', type_id: 1}, { grade: 'V3', type_id: 1}, { grade: 'V4', type_id: 1}, { grade: 'V5', type_id: 1}, { grade: 'V6', type_id: 1}, { grade: 'V7', type_id: 1}, { grade: 'V8', type_id: 1}, { grade: 'V9', type_id: 1}, { grade: 'V10', type_id: 1}, { grade: 'V11', type_id: 1}, { grade: 'V12', type_id: 1}, { grade: 'V13', type_id: 1}, { grade: 'V14', type_id: 1}, { grade: 'V15', type_id: 1}, { grade: '5.6'}, { grade: '5.7'}, { grade: '5.8'}, { grade: '5.9'}, { grade: '5.10a'}, { grade: '5.10b'}, { grade: '5.10c'}, { grade: '5.10d'}, { grade: '5.11a'}, { grade: '5.11b'}, { grade: '5.11c'}, { grade: '5.11d'}, { grade: '5.12a'}, { grade: '5.12b'}, { grade: '5.12c'}, { grade: '5.12.d'}, { grade: '5.13a'}, { grade: '5.13b'}, { grade: '5.13c'}, { grade: '5.13d'}, { grade: '5.14a'}, { grade: '5.14b'}, { grade: '5.14c'}, { grade: '5.14d'}, { grade: '5.15a'}, { grade: '5.15b'}, { grade: '5.15c'}, { grade: '5.15d'}])
-
-# types = Type.create([ { route_type: 'Boulder' }, { route_type: 'Lead' }, { route_type: 'Toprope' }])
-
-# route1 = Route.create([{ grade_id: 21, type_id: 2, user_id: 1, name: 'A Beautiful Life', country: 'USA', crag: 'Sinks Canyon', comments: 'Left line on the small slab towards the far left side of Addiction Wall. 8 bolts.', coordinates: '', gym_route: false}])
-# route2 = Route.create([{ grade_id: 10, type_id: 1, user_id: 1, name: 'A Big Round of Applause', country: 'USA', crag: 'Bishop', comments: 'Traverse up and to the right until the center, then straight up over the overhang.', coordinates: '', gym_route: false}])
+    )
+end
 
 
-# Data above has been seeded.
+grades = Grade.create([{ grade: 'VB', type_id: 1}, { grade: 'V0', type_id: 1}, { grade: 'V1', type_id: 1}, { grade: 'V2', type_id: 1}, { grade: 'V3', type_id: 1}, { grade: 'V4', type_id: 1}, { grade: 'V5', type_id: 1}, { grade: 'V6', type_id: 1}, { grade: 'V7', type_id: 1}, { grade: 'V8', type_id: 1}, { grade: 'V9', type_id: 1}, { grade: 'V10', type_id: 1}, { grade: 'V11', type_id: 1}, { grade: 'V12', type_id: 1}, { grade: 'V13', type_id: 1}, { grade: 'V14', type_id: 1}, { grade: 'V15', type_id: 1}, { grade: '5.6'}, { grade: '5.7'}, { grade: '5.8'}, { grade: '5.9'}, { grade: '5.10a'}, { grade: '5.10b'}, { grade: '5.10c'}, { grade: '5.10d'}, { grade: '5.11a'}, { grade: '5.11b'}, { grade: '5.11c'}, { grade: '5.11d'}, { grade: '5.12a'}, { grade: '5.12b'}, { grade: '5.12c'}, { grade: '5.12.d'}, { grade: '5.13a'}, { grade: '5.13b'}, { grade: '5.13c'}, { grade: '5.13d'}, { grade: '5.14a'}, { grade: '5.14b'}, { grade: '5.14c'}, { grade: '5.14d'}, { grade: '5.15a'}, { grade: '5.15b'}, { grade: '5.15c'}, { grade: '5.15d'}])
 
-# routes = Route.create([{ grade_id: int, type_id: int, user_id: int, name: 'string', country: 'string', crag: 'string', comments: 'text', coordinates: string, gym_route: boolean, date_created: date }])
+types = Type.create([ { route_type: 'Boulder' }, { route_type: 'Lead' }, { route_type: 'Toprope' }])
+
+route1 = Route.create([{ grade_id: 21, type_id: 2, user_id: 1, name: 'A Beautiful Life', country: 'USA', crag: 'Sinks Canyon', comments: 'Left line on the small slab towards the far left side of Addiction Wall. 8 bolts.', coordinates: '', gym_route: false}])
+route2 = Route.create([{ grade_id: 10, type_id: 1, user_id: 1, name: 'A Big Round of Applause', country: 'USA', crag: 'Bishop', comments: 'Traverse up and to the right until the center, then straight up over the overhang.', coordinates: '', gym_route: false}])
+
+
+Data above has been seeded.
+
+routes = Route.create([{ grade_id: int, type_id: int, user_id: int, name: 'string', country: 'string', crag: 'string', comments: 'text', coordinates: string, gym_route: boolean, date_created: date }])
